d7e58d9f93a878d6da6272a2cf0d4baf0417e618
xeounxzxu <pnw123456@icloud.com>
2025-05-11 11:21:28 +0900

    build: project add

---
+++ src/main/kotlin/io/xeounxzxu/lecturemanagementapi/LectureManagementApiApplication.kt
@@ -0,0 +1,11 @@
+package io.xeounxzxu.lecturemanagementapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LectureManagementApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<LectureManagementApiApplication>(*args)
+}
---
+++ src/test/kotlin/io/xeounxzxu/lecturemanagementapi/LectureManagementApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.xeounxzxu.lecturemanagementapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LectureManagementApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
