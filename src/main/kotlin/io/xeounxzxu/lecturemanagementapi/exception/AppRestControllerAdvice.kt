fced229381809a91602984e8cd6d78153d73abfc
xeounxzxu <pnw123456@icloud.com>
2025-05-11 14:51:26 +0900

    chore: 에러 규칙 추가

--- src/main/kotlin/io/xeounxzxu/lecturemanagementapi/exception/AppRestControllerAdvice.kt
+++ src/main/kotlin/io/xeounxzxu/lecturemanagementapi/exception/AppRestControllerAdvice.kt
@@ -4,6 +4,7 @@ import io.xeounxzxu.lecturemanagementapi.exception.ErrorResponse.Companion.DEFAU
 import mu.KotlinLogging
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -15,10 +16,24 @@ private val log = KotlinLogging.logger {}
 @RestControllerAdvice
 class AppRestControllerAdvice {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun error(
+        ex: HttpMessageNotReadableException
+    ): ResponseEntity<ErrorResponse> {
+        log.error(ex) { ex.message }
+        return ResponseEntity.of(
+            Optional.of(
+                ErrorResponse.toBadRequest()
+            )
+        )
+    }
+
     @ExceptionHandler(BusinessException::class)
     fun error(
         ex: BusinessException
     ): ResponseEntity<ErrorResponse> {
+        log.error(ex) { ex.message }
         return ResponseEntity.status(ex.status)
             .body(ErrorResponse(ex.message ?: DEFAULT_ERROR))
     }
